04ad040724b88134d2261c383c45e661c595d3b5
maeve <maeve.revels@g5platform.com>
2011-09-28 08:20:02 -0700

    Generate basic gemspec

--- lib/adapter_generator/new_gem.rb
+++ lib/adapter_generator/new_gem.rb
@@ -8,17 +8,48 @@ module AdapterGenerator
     desc 'Generates a skeletal project for a new third-party adapter'
     argument :name, :desc => 'The name of the new adapter'
 
+    class_option :author_name, :type => :string, :desc => "the gem author's name (defaults to git config user.name)"
+    class_option :author_email, :type => :string, :desc => "the gem author's email (defaults to git config user.email)"
+    class_option :homepage, :type => :string, :desc => "the homepage URL for the project"
+    class_option :bin, :type => :boolean, :default => false, :aliases => "-b", :banner => "Generate a binary for your library"
+
     def self.source_root
       File.expand_path(File.join(File.dirname(__FILE__), 'templates'))
     end
 
     def create_lib
       opts = {:name => name.underscore, :constant => name.camelize}
-
-      target = File.join(Dir.pwd, opts[:name], 'lib')
+      target = File.join(gem_path, 'lib')
 
       template(File.join('lib', 'new_gem.rb.tt'), File.join(target, "#{opts[:name]}.rb"), opts)
       template(File.join('lib', 'new_gem', 'version.rb.tt'), File.join(target, opts[:name], 'version.rb'), opts)
+    end
+
+    def create_gemspec
+      opts = {:name => name.underscore,
+              :constant => name.camelize,
+              :author_name => options[:author_name] || git_user_name,
+              :author_email => options[:author_email] || git_user_email,
+              :homepage => options[:homepage],
+              :bin => options[:bin] }
+
+      opts[:author_name] = "TODO: enter your name" if opts[:author_name].blank?
+      opts[:author_email] = "TODO: enter your email address" if opts[:author_email].blank?
+
+      template('new_gem.gemspec.tt', File.join(gem_path, "#{opts[:name]}.gemspec"), opts)
+    end
+
+    private
+    def gem_path
+      File.join(Dir.pwd, name.underscore)
+    end
+
+    def git_user_name
+      `git config user.name`.chomp
+    end
+
+    def git_user_email
+      `git config user.email`.chomp
     end
   end
 end
--- spec/adapter_generator/new_gem_spec.rb
+++ spec/adapter_generator/new_gem_spec.rb
@@ -4,10 +4,18 @@ describe AdapterGenerator::NewGem do
   let(:project_root) { File.expand_path(File.join(File.dirname(__FILE__), '..', '..')) }
   before { FakeFS::FileSystem.clone(project_root) }
 
+  let(:author_name) { '' }
+  let(:author_email) { '' }
+
+  before do
+    AdapterGenerator::NewGem.any_instance.stub(:git_user_name).and_return(author_name)
+    AdapterGenerator::NewGem.any_instance.stub(:git_user_email).and_return(author_email)
+  end
+
   let(:run_generator) { capture(:stdout) { AdapterGenerator::NewGem.start(args) } }
   subject { run_generator }
 
-  let(:args) { [gem_name_arg] }
+  let(:args) { gem_name_arg }
 
   let(:gem_path) { File.expand_path(gem_name) }
   let(:lib_path) { File.join(gem_path, 'lib') }
@@ -46,12 +54,98 @@ describe AdapterGenerator::NewGem do
     end
   end
 
+  shared_examples_for 'a gemspec generator' do
+    let(:gemspec_file) { File.join(gem_path, "#{gem_name}.gemspec") }
+
+    it "should create a file for the gemspec" do
+      expect { subject }.to change { File.file?(gemspec_file) }.from(false).to(true)
+    end
+
+    describe "the gemspec" do
+      before { run_generator }
+
+      subject { File.open(gemspec_file, 'r') { |f| f.read } }
+
+      it { should match /s\.name\s*=\s*"#{gem_name}"/ }
+      it { should match /s\.version\s+=\s*#{module_name}::VERSION/ }
+
+      context "without --author_name" do
+        context "when the author is in the git config" do
+          let(:author_name) { 'Jane Q. Developer' }
+
+          it { should match /s\.authors\s*=\s*\[\s*"#{author_name}"\s*\]/ }
+        end
+
+        context "when there is no author in the git config" do
+          it { should match /s\.authors\s*=\s*\[\s*".*TODO.*"\s*\]/ }
+        end
+      end
+
+      context "with --author_name" do
+        let(:args) { [gem_name, "--author_name=#{author_name}"] }
+        let(:author_name) { 'G5' }
+
+        it { should match /s\.authors\s*=\s*\[\s*"#{author_name}"\s*\]/ }
+      end
+
+      context "without --author_email" do
+        context "when the author email is in the git config" do
+          let(:author_email) { 'jane@test.com' }
+
+          it { should match /s\.email\s*=\s*\[\s*"#{author_email}"\s*\]/ }
+        end
+
+        context "when there is no author email in the git config" do
+          it { should match /s\.email\s*=\s*\[\s*".*TODO.*"\s*\]/ }
+        end
+      end
+
+      context "with --author_email" do
+        let(:args) { [gem_name, "--author_email=#{author_email}"] }
+        let(:author_email) { 'engineering@g5platform.com' }
+
+        it { should match /s\.email\s*=\s*\[\s*"#{author_email}"\s*\]/ }
+      end
+
+      it { should match /s\.summary\s*=\s*%q\{.*TODO.*\}/ }
+      it { should match /s\.description\s*=\s*%q\{.*TODO.*\}/ }
+
+      context "without --homepage" do
+        it { should match /s\.homepage\s*=\s*""/ }
+      end
+
+      context "with --homepage" do
+        let(:homepage) { 'http://testing.com/my_gem' }
+        let(:args) { [gem_name, "--homepage=#{homepage}"] }
+        it { should match /s\.homepage\s*=\s*"#{homepage}"/ }
+      end
+
+      it { should match /s\.rubyforge_project\s*=\s*"#{gem_name}"/ }
+
+      it { should match /s\.files\s*=/ }
+      it { should match /s\.test_files\s*=/ }
+
+      context "without --bin" do
+        it { should_not match /s\.executables\s*=/ }
+      end
+
+      context "with --bin" do
+        let(:args) { [gem_name, '--bin'] }
+
+        it { should match /s\.executables\s*=/ }
+      end
+
+      it { should match /s\.require_paths\s*=\s*\["lib"\]/ }
+    end
+  end
+
   context "when gem name argument is in snake case" do
     let(:gem_name_arg) { 'my_gem' }
     let(:gem_name) { gem_name_arg }
     let(:module_name) { 'MyGem' }
 
     it_should_behave_like 'a gem lib generator'
+    it_should_behave_like 'a gemspec generator'
   end
 
   context "when gem name argument is in camel case" do
@@ -60,6 +154,8 @@ describe AdapterGenerator::NewGem do
     let(:module_name) { gem_name_arg }
 
     it_should_behave_like 'a gem lib generator'
+    it_should_behave_like 'a gemspec generator'
+
   end
 
   context "when gem name argument is in a hybrid format" do
@@ -68,6 +164,8 @@ describe AdapterGenerator::NewGem do
     let(:module_name) { 'MyGemIsAwesome' }
 
     it_should_behave_like 'a gem lib generator'
+    it_should_behave_like 'a gemspec generator'
+
   end
 
 
